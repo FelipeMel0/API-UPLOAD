b7b4f6df3a1a627b0fe060b80e9193eba9ed9f2b
Mayssa Martha <felipedemfernandes@gmail.com>
2022-05-19 11:28:49 -0300

    Cadastrando título de livro

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const express = require('express')
 const fs = require('fs')
+const Livro = require('./models/Livro')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true, limit: '5MB'}))
 
-app.post('/testeUpload', (req, res)=>{
+app.post('/testeUpload', (req, res) => {
 
     let buffer = new Buffer.from(req.body.file, 'base64')
     let imageName = './uploads/' + Date.now().toString() + '.jpg'
 
+    let titulo = req.body.titulo
+
     // console.log(buffer)
 
-    
     fs.writeFileSync(imageName, buffer, 'base64', (error)=>{
 
         if(error) console.log(error)
 
     })
 
-    res.status(200)
+    const livro = Livro.create({
+        titulo: titulo,
+        imagem: imageName
+    }).then(() => {
+        res.status(200).send(livro)
+    })
+
+    // res.status(200).send()
 
 })
 
